a90be512f682fc17b9c828cc3a44921395da961c
TSPsoftware <92826085+TSPsoftware@users.noreply.github.com>
2022-04-17 18:27:53 -0300

    Add files via upload

---
+++ Mulher-Maravilha-1984.js
@@ -0,0 +1,30 @@
+async function enviarScript(scriptText){
+    const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line),
+  main = document.querySelector("#main"),
+  textarea = main.querySelector(`div[contenteditable="true"]`)
+  
+  if(!textarea) throw new Error("Não há uma conversa aberta")
+  
+  for(const line of lines) {
+    console.log(line)
+  
+    textarea.textContent = line
+    textarea.dispatchEvent(new InputEvent("input", { bubbles: true }));
+  
+    (main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click()
+    
+    if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250))
+  }
+  
+  return lines.length
+  }
+
+  let texto = "";
+
+  for(let i = 1; i < 1984; i++){
+    texto += `\nMulher-Maravilha ${i}`
+  }
+  
+  enviarScript(`Galera, para entender Mulher-Maravilha 1984 eu vou ter que assistir
+  ${texto}
+  ?`).then(e => console.log(`Código finalizado, ${e} mensagens enviadas`)).catch(console.error)
